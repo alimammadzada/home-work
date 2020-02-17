d63810a67414c04a44ea3808ff6041fc638cde98
Ali Mammadzada <ali.mammadzada.std@bhos.edu.az>
2020-02-18 02:15:42 +0400

    Homework 3 has been done.

--- src/main/java/hw03/HomeWork3.java
+++ src/main/java/hw03/HomeWork3.java
@@ -13,38 +13,38 @@ public class HomeWork3 {
         schedule[2][0] = "Tuesday";
         schedule[2][1] = "go to party";
         schedule[3][0] = "Wednesday";
-        schedule[3][1] = "visit the teaher";
+        schedule[3][1] = "visit the teacher";
         schedule[4][0] = "Thursday";
         schedule[4][1] = "buy books";
         schedule[5][0] = "Friday";
-        schedule[5][1] = "do houseworks";
+        schedule[5][1] = "do house works";
         schedule[6][0] = "Saturday";
         schedule[6][1] = "do sports";
         //loopK
         for (; ; ) {
-            String userInput = s.nextLine().toLowerCase().trim();
-            if (userInput.equals("sunday")) {
+            String input = s.nextLine().toLowerCase().trim();
+            if (input.equals("sunday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[0][0], schedule[0][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("monday")) {
+            } else if (input.equals("monday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[1][0], schedule[1][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("tuesday")) {
+            } else if (input.equals("tuesday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[2][0], schedule[2][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("wednesday")) {
+            } else if (input.equals("wednesday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[3][0], schedule[3][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("thursday")) {
+            } else if (input.equals("thursday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[4][0], schedule[4][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("friday")) {
+            } else if (input.equals("friday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[5][0], schedule[5][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if (userInput.equals("saturday")) {
+            } else if (input.equals("saturday")) {
                 System.out.printf("Your tasks for %s: %s\n", schedule[6][0], schedule[6][1]);
                 System.out.print("Please, input the day of the week: ");
-            } else if(userInput.equals("exit")){
+            } else if(input.equals("exit")){
                 break;
             }
             else {
